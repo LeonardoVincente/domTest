899f886b0316cb6a20fe6b1c908f77ef83f61314
lehevi <leonzkyhv@gmail.com>
2019-07-25 22:12:08 -0700

    Basic layout and webpage
    Basic layoout and webpage test

---
+++ page/main.js
@@ -0,0 +1,19 @@
+
+function init(){
+    setTimeout( ()=>{
+        // document.getElementById('mainCont').appendChild(popup());
+        document.getElementById('dropdown').classList.add("show_dropdown");;
+    }, 3000);    
+}
+
+function popup(){
+    let popup = document.createElement('div');
+    popup.setAttribute('class', 'popUp');
+    let createMessage = document.createElement('div');
+    createMessage.setAttribute('class', 'message_container');
+    createMessage.textContent = "This is a test";
+    popup.appendChild(createMessage);
+    return popup;
+}
+
+window.onload = init;
